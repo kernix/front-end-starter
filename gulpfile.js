53527d6fe5ac9b2bdce2939dd468c067c0df0362
Jean-Bernard PHAM <berlouz2405@gmail.com>
2016-03-23 21:47:07 +0100

    Update Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
   sourcemaps = require('gulp-sourcemaps'),
   plumber = require('gulp-plumber'),
   postcss = require('gulp-postcss'),
+  cleancss = require('gulp-cleancss'),
   autoprefixer = require('autoprefixer'),
   perfectionist = require('perfectionist'),
   concat = require('gulp-concat'),
@@ -29,7 +30,7 @@ gulp.task('bower', function () {
 
 // Style
 gulp.task('styles', function() {
-  return gulp.src('./less/styles.less')
+  return gulp.src('src/theme/less/theme.less')
     .pipe(sourcemaps.init())
     .pipe(plumber({
       errorHandler: function(err) {
@@ -43,7 +44,7 @@ gulp.task('styles', function() {
         browsers: ['last 3 version']
       })
     ]))
-    // .pipe(cleancss())
+    .pipe(cleancss())
     .pipe(sourcemaps.write('./map'))
     .pipe(gulp.dest('css'));
 });
@@ -79,4 +80,4 @@ gulp.task('watch', function() {
 });
 
 // Default
-gulp.task('default', ['styles', 'watch', 'bower']);
+gulp.task('default', ['styles', 'watch', 'bower' ]);
