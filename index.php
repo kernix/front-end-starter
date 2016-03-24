7f558eb9d908f3b15b64bd96297c19272159444c
JB PHAM <jbpham@kernix.com>
2016-03-24 12:34:05 +0100

    Update bower

--- index.php
+++ index.php
@@ -21,7 +21,6 @@ $include = isset($page) ? 'www/templates/pages/'.$page.'.php' : 'www/templates/_
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
   <meta name="description" content="<?=$site->desc?>">
   <!-- Styles -->
-  <link rel="stylesheet" href="/public/vendor/font-awesome/css/font-awesome.min.css" />
   <link rel="stylesheet" href="/public/css/theme.css" />
   <!-- Favicon -->
   <link rel="apple-touch-icon" sizes="57x57" href="/public/img/favicons/apple-touch-icon-57x57.png" />
@@ -31,11 +30,11 @@ $include = isset($page) ? 'www/templates/pages/'.$page.'.php' : 'www/templates/_
   <link rel="icon" type="image/png" href="/public/img/favicons/favicon-32x32.png" sizes="32x32" />
   <link rel="icon" type="image/png" href="/public/img/favicons/favicon-96x96.png" sizes="96x96" />
   <link rel="icon" type="image/png" href="/public/img/favicons/favicon-16x16.png" sizes="16x16" />
-  <link rel="manifest" href="img/favicons/manifest.json" />
-  <link rel="mask-icon" href="img/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-  <link rel="shortcut icon" href="img/favicons/favicon.ico" />
+  <link rel="manifest" href="/public/img/favicons/manifest.json" />
+  <link rel="mask-icon" href="/public/img/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+  <link rel="shortcut icon" href="/public/img/favicons/favicon.ico" />
   <meta name="msapplication-TileColor" content="#2b5797" />
-  <meta name="msapplication-config" content="img/favicons/browserconfig.xml" />
+  <meta name="msapplication-config" content="/public/img/favicons/browserconfig.xml" />
   <meta name="theme-color" content="#ffffff" />
 </head>
 <body class="page-<?=isset($page) ? ($page == 'home' ? 'home' : $page.' not-home') : 'template'?>">
@@ -46,7 +45,12 @@ include 'www/templates/_footer.php';
 ?>
   <script src="/public/vendor/jquery/dist/jquery.min.js"></script>
   <script src="/public/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="/public/vendor/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+  <script src="/public/vendor/moment/moment.js"></script>
+  <script src="/public/vendor/jquery-placeholder/jquery.placeholder.min.js"></script>
+  <script src="/public/vendor/sweetalert/dist/sweetalert.min.js"></script>
+  <script src="/public/js/custom-file-input.js"></script>
   <script src="/public/js/all.js"></script>
-  <script src="/public/js/main.js"></script>
+  <script src="/public/vendor/js/bundle.js"></script>
 </body>
 </html>
