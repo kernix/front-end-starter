ff9216d758cca78861a6a276b7ae91bc79c2ec60
Berlouz <Berlouz@MacBook-Pro.local>
2016-05-22 10:09:05 +0200

    Update package.json + add swipe

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,40 @@
 var $ = require('jquery');
 
+// Hammer js addon
+(function(factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery', 'hammer'], factory);
+  } else if (typeof exports === 'object') {
+    factory(require('jquery'), require('hammer'));
+  } else {
+    factory(jQuery, Hammer);
+  }
+}(function($, Hammer) {
+  function hammerify(el, options) {
+    var $el = $(el);
+    if(!$el.data("hammer")) {
+      $el.data("hammer", new Hammer($el[0], options));
+    }
+  }
+
+  $.fn.hammer = function(options) {
+    return this.each(function() {
+      hammerify(this, options);
+    });
+  };
+
+  // extend the emit method to also trigger jQuery events
+  Hammer.Manager.prototype.emit = (function(originalEmit) {
+    return function(type, data) {
+      originalEmit.call(this, type, data);
+      $(this.element).trigger({
+        type: type,
+        gesture: data
+      });
+    };
+  })(Hammer.Manager.prototype.emit);
+}));
+
 $(document).ready(function () {
   // Custom functions
   var isMobile = false;
@@ -175,5 +210,15 @@ $(document).ready(function () {
       .on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
     });
 
+
+    // Swipe Carousel
+    $(function() {
+      $('.carousel').hammer().on('swipeleft', function(){
+        $(this).carousel('next');
+      })
+      $('.carousel').hammer().on('swiperight', function(){
+        $(this).carousel('prev');
+      })
+    });
   });
 });
