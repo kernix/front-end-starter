b595d8dc9e390c9bb3e8707fa7418e87d01724d7
Jean-Bernard PHAM <jbpham@kernix.com>
2017-02-19 11:26:00 +0100

    Update gulp less web pack omg bower

---
+++ src/gulp/webpack.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+
+module.exports = function (gulp, plugins, name, dest) {
+  return function (done) {
+    let entry = {};
+    entry[name] = './js/' + name + '.js';
+    plugins.webpack({
+      'entry': entry,
+      'output': {
+        'filename': dest + '/[name].min.js'
+      },
+      'plugins': [
+        new webpack.optimize.UglifyJsPlugin(),
+      ],
+      externals: {
+        'jquery': 'jQuery',
+        'hammer': 'Hammer'
+      }
+    }, function (err, stats) {
+      if (err) throw new plugins.util.PluginError('webpack', err);
+      plugins.util.log('[webpack:' + name + ']', stats.toString());
+      done();
+    });
+  };
+};
