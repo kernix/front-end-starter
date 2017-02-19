c5d7982caed6169779240adee5f55f65d7b996e5
Jean-Bernard PHAM <jbpham@kernix.com>
2017-02-19 14:55:37 +0100

    Update favicon + icon font

--- src/gulp/favicons.js
+++ src/gulp/favicons.js
@@ -1,1 +1,64 @@
-// Provisoire
+var FAVICON_DATA_FILE = 'faviconData.json';
+var realFavicon = require ('gulp-real-favicon');
+
+module.exports = function (gulp, plugins) {
+  return function (done) {
+  realFavicon.generateFavicon({
+      masterPicture: 'img/favicon/favicon.png',
+      dest: '../dist/img/favicons/',
+      iconsPath: '../dist/img/favicons/',
+      design: {
+        ios: {
+          pictureAspect: 'noChange',
+          assets: {
+            ios6AndPriorIcons: false,
+            ios7AndLaterIcons: false,
+            precomposedIcons: false,
+            declareOnlyDefaultIcon: true
+          }
+        },
+        desktopBrowser: {},
+        windows: {
+          pictureAspect: 'noChange',
+          backgroundColor: '#2b5797',
+          onConflict: 'override',
+          assets: {
+            windows80Ie10Tile: false,
+            windows10Ie11EdgeTiles: {
+              small: false,
+              medium: true,
+              big: false,
+              rectangle: false
+            }
+          }
+        },
+        androidChrome: {
+          pictureAspect: 'noChange',
+          themeColor: '#ffffff',
+          manifest: {
+            display: 'standalone',
+            orientation: 'notSet',
+            onConflict: 'override',
+            declared: true
+          },
+          assets: {
+            legacyIcon: false,
+            lowResolutionIcons: false
+          }
+        },
+        safariPinnedTab: {
+          pictureAspect: 'blackAndWhite',
+          threshold: 49.21875,
+          themeColor: '#5bbad5'
+        }
+      },
+      settings: {
+        scalingAlgorithm: 'Mitchell',
+        errorOnImageTooSmall: false
+      },
+      markupFile: FAVICON_DATA_FILE
+    }, function() {
+      done();
+    });
+  };
+};
--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 
+var iconfont = require('gulp-iconfont');
+var iconfontCss = require('gulp-iconfont-css');
+var runTimestamp = Math.round(Date.now()/1000);
+
 var plugins = require('gulp-load-plugins')({
   'config': require('./package.json'),
   'pattern': ['*'],
@@ -32,3 +36,30 @@ gulp.task('webpack:main', require('./gulp/webpack')(gulp, plugins, 'main', '../d
 gulp.task('img', require('./gulp/img')(gulp, plugins));
 
 gulp.task('font:typo', require('./gulp/fontTypo')(gulp, plugins));
+// gulp.task('font:icon', require('./gulp/fontIcon')(gulp, plugins));
+
+gulp.task('favicon', require('./gulp/favicons')(gulp, plugins));
+
+
+var fontName = 'Eleven';
+
+gulp.task('Iconfont', function(){
+  return gulp.src(['./fonts/icons/*.svg'])
+    .pipe(iconfontCss({
+       fontName: fontName,
+       path: './less/mixins/iconTmp.less',
+       targetPath: '/../../../src/less/elements/icons.less',
+       fontPath: '../fonts/icons/'
+     }))
+    .pipe(iconfont({
+      fontName: fontName, // required
+      prependUnicode: true, // recommended option
+      formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'], // default, 'woff2' and 'svg' are available
+      timestamp: runTimestamp, // recommended to get consistent builds when watching files
+    }))
+      .on('glyphs', function(glyphs, options) {
+        // CSS templating, e.g.
+        console.log(glyphs, options);
+      })
+    .pipe(gulp.dest('../dist/fonts/icons/'));
+});
