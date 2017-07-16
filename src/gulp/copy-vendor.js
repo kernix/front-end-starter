812c2b2dff1b79193b2a1fb030c9a5d5a7400958
jbphamkernix <jbpham@kernix.com>
2017-07-16 14:57:54 +0200

    Delete Bower

---
+++ src/gulp/copy-vendor.js
@@ -0,0 +1,32 @@
+module.exports = function (gulp, plugins) {
+  return function () {
+    return gulp.src([
+      // Bootstrap
+      './node_modules/bootstrap/dist/js/bootstrap.min.js',
+      './node_modules/bootstrap/dist/fonts/*',
+      // AOS
+      './node_modules/aos/dist/aos.css',
+      './node_modules/aos/dist/aos.js',
+      // Bootstrap datetimepicker
+      './node_modules/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+      // Font-awesome
+      './node_modules/font-awesome/css/font-awesome.min.css',
+      './node_modules/font-awesome/fonts/*',
+      // Hammer
+      './node_modules/hammerjs/hammer.min.js',
+      './node_modules/hammerjs/hammer.min.js.map',
+      // Jquery
+      './node_modules/jquery/dist/jquery.min.js',
+      // Material design icon
+      './node_modules/material-design-iconic-font/dist/css/material-design-iconic-font.min.css',
+      './node_modules/material-design-iconic-font/dist/fonts/*',
+      // Moment
+      './node_modules/moment/locale/fr.js',
+      './node_modules/moment/min/moment.min.js',
+      // Rellax
+      './node_modules/rellax/rellax.min.js'
+    ], {
+        base: 'node_modules'
+    }).pipe(gulp.dest('../dist/vendor/'));
+  };
+};
