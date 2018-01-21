8ffdcc67077dc8ca748a2969d58b99fef9b9ea38
jbphamkernix <jbpham@kernix.com>
2018-01-21 20:08:04 +0100

    Update sourcemap prod & dev

--- src/gulp/sass.js
+++ src/gulp/sass.js
@@ -1,7 +1,13 @@
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+var config = {
+  sourceMaps: !gutil.env.production
+};
+
 module.exports = function (gulp, plugins, name, dest, reload) {
   return function () {
     return gulp.src('./sass/' + name + '.scss')
-      .pipe(plugins.sourcemaps.init())
+      .pipe(gulpif(config.sourceMaps, plugins.sourcemaps.init()))
       .pipe(plugins.sass().on('error', plugins.sass.logError))
       .pipe(plugins.postcss([
         new plugins.autoprefixer({
@@ -10,7 +16,7 @@ module.exports = function (gulp, plugins, name, dest, reload) {
       ]))
       .pipe(plugins.cleanCss())
       .pipe(plugins.rename(name + '.min.css'))
-      .pipe(plugins.sourcemaps.write('./map'))
+      .pipe(gulpif(config.sourceMaps, plugins.sourcemaps.write('./map')))
       .pipe(gulp.dest(dest))
   };
 };
