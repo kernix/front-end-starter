eb845ddb612463ec65f62854fdece1e47ba0ba3a
jbphamkernix <jbpham@kernix.com>
2018-02-10 21:31:11 +0100

    Update webpack

--- index.php
+++ index.php
@@ -13,14 +13,11 @@ $include = isset($page) ? 'www/templates/pages/'.$page.'.php' : 'www/templates/_
 <head>
   <meta charset="UTF-8">
   <title><?=$site->title?></title>
-  <?php if ($is_responsive): ?>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <?php endif; ?>
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
   <meta name="description" content="<?=$site->desc?>">
   <!-- Styles -->
-  <link rel="stylesheet" href="dist/vendor/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="dist/css/theme.min.css">
+  <link rel="stylesheet" href="dist/css/main.min.css">
   <!-- Favicon -->
   <link rel="apple-touch-icon" sizes="57x57" href="dist/img/favicons/apple-touch-icon-57x57.png">
   <link rel="apple-touch-icon" sizes="60x60" href="dist/img/favicons/apple-touch-icon-60x60.png">
@@ -48,12 +45,6 @@ include 'www/templates/_header.php';
 include $include;
 include 'www/templates/_footer.php';
 ?>
-<script src="dist/vendor/jquery/dist/jquery.min.js"></script>
-<script src="dist/vendor/moment/min/moment.min.js"></script>
-<script src="dist/vendor/moment/locale/fr.js"></script>
-<script src="dist/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="dist/vendor/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-<script src="dist/vendor/hammerjs/hammer.min.js"></script>
 <script src="dist/js/main.min.js"></script>
 </body>
 </html>
--- src/gulpfile.js
+++ src/gulpfile.js
@@ -6,26 +6,30 @@ var plugins = require('gulp-load-plugins')({
   'scope': ['dependencies','devDependencies']
 });
 
-gulp.task('less:front', require('./tasks/less')(gulp, plugins, 'theme', '../dist/css', false));
+gulp.task('less:front', require('./tasks/less')(gulp, plugins, 'main', '../dist/css', false));
 gulp.task('webpack:main', require('./tasks/webpack')(gulp, plugins, 'main', '../dist/js'));
 gulp.task('copy-vendor', require('./tasks/copy-vendor')(gulp, plugins));
 
 // Optional
-// gulp.task('iconfont', require('./tasks/iconfont')(gulp, plugins));
+gulp.task('iconfont', require('./tasks/iconfont')(gulp, plugins));
 //
 // gulp.task('img', require('./tasks/img')(gulp, plugins));
 //
 // gulp.task('font:typo', require('./tasks/fontTypo')(gulp, plugins));
 //
-// gulp.task('favicon:img', require('./tasks/favicons')(gulp, plugins));
-// gulp.task('favicon:code', require('./tasks/faviconsCode')(gulp, plugins));
+gulp.task('favicon:img', require('./tasks/favicons')(gulp, plugins));
+gulp.task('favicon:code', require('./tasks/faviconsCode')(gulp, plugins));
 
 
+// Main
 gulp.task('less', gulp.parallel('less:front'));
 
 gulp.task('webpack', gulp.parallel('webpack:main'));
+
 // gulp.task('favicon', gulp.parallel('favicon:img', 'favicon:code'));
 
+
+// Watch
 gulp.task('watch:less', function () {
   gulp.watch('./less/**/*.less', gulp.series('less'));
 });
@@ -37,6 +41,6 @@ gulp.task('watch:iconfont', function () {
 });
 
 
-gulp.task('watch', gulp.parallel('less', 'webpack', 'watch:less', 'watch:js'/*, 'watch:iconfont'*/));
+gulp.task('watch', gulp.parallel('less', 'webpack', 'watch:less', 'watch:js', 'watch:iconfont'));
 
 gulp.task('default', gulp.parallel('copy-vendor', 'less', 'webpack'));
