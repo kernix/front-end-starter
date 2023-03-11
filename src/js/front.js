4beaeab1486ab50882bd33875fef399ec90ec1a9
Jb PHAM <jbpham@kernix.com>
2023-03-11 11:54:02 +0100

    Add inte search

--- src/js/front.js
+++ src/js/front.js
@@ -238,7 +238,19 @@ ready(() => {
   //   }
   // }
 
-
+  // // Search Toggle
+  // const btnSearch = document.querySelectorAll('.btn-toggle-search');
+  // btnSearch.forEach(thisBtnSearch => {
+  //   thisBtnSearch.addEventListener('click', event => {
+  //     window.setTimeout(function () { 
+  //       document.querySelector('.header-search-form .form-control').focus(); 
+  //     }, 0); 
+  //     document.querySelector('.header-search-form').slideToggle();
+  //     document.querySelector('.header-search-overlay').classList.toggle('active');
+  //     document.querySelector('html').classList.toggle('overflow-search');
+  //     document.querySelector('body').classList.toggle('overflow-search');
+  //   });
+  // });
 
 });
 
