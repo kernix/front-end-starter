260263aaa79282950775f509400fa4548c15e391
Jb PHAM <jbpham@kernix.com>
2023-03-12 11:56:58 +0100

    Create cms.sh

---
+++ src/cms.sh
@@ -0,0 +1,15 @@
+#bin bash
+RED=$'\e[0;31m'
+GREEN=$'\e[0;32m'
+NC=$'\e[0m'
+# main.scss
+sed -i '' -e '56,63 s|^// ||' sass/main.scss
+
+# # front.js
+sed -i '' -e '2,3 s|^// ||' js/front.js
+sed -i '' -e '67,248 s|^  // ||' js/front.js
+
+# main.js
+sed -i '' -e '2 s|^// ||' js/main.js
+
+echo "${GREEN}CMS sass and js is activated ${NC}\n"
