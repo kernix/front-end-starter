10a667f9da1c514eb94260c43571c7c65a1585f2
Jb PHAM <jbpham@kernix.com>
2023-06-16 19:53:55 +0200

    Update cms

--- src/cms.sh
+++ src/cms.sh
@@ -3,14 +3,14 @@ RED=$'\e[0;31m'
 GREEN=$'\e[0;32m'
 NC=$'\e[0m'
 # main.scss
-sed -i '' -e '56,65 s|^// ||' sass/main.scss
+sed -i '' -e '56,6 s|^// ||' sass/main.scss
 
 # front.js
 sed -i '' -e '2,3 s|^// ||' js/front.js
 sed -i '' -e '67,293 s|^  // ||' js/front.js
 
 # copy-vendor.js
-sed -i '' -e '8,13 s|^  // ||' tasks/copy-vendor.js
+sed -i '' -e '8,13 s|^// ||' tasks/copy-vendor.js
 
 # main.js
 sed -i '' -e '2 s|^// ||' js/main.js
