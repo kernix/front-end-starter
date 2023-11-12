c8505010c507617d59d35f853e61795c896721cb
Jb PHAM <jbpham@kernix.com>
2023-11-12 19:50:22 +0100

    Update cms.sh

--- src/cms.sh
+++ src/cms.sh
@@ -5,15 +5,12 @@ NC=$'\e[0m'
 # main.scss
 sed -i '' -e '60,74 s|^// ||' sass/main.scss
 
-# front.js
-sed -i '' -e '2,3 s|^// ||' js/front.js
-sed -i '' -e '19,27 s|^// ||' js/front.js
-sed -i '' -e '46,58 s|^    // ||' js/front.js
-
 # copy-vendor.js
 sed -i '' -e '8,13 s|^// ||' tasks/copy-vendor.js
 
 # main.js
 sed -i '' -e '2 s|^// ||' js/main.js
+sed -i '' -e '19,27 s|^// ||' js/main.js
+sed -i '' -e '46,58 s|^    // ||' js/main.js
 
 echo "${GREEN}CMS sass and js is activated ${NC}\n"
