9e3549636c6a916268a6094f2246ec98846d1425
Jb PHAM <jbpham@kernix.com>
2023-11-20 20:54:04 +0100

    Update js

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -2,24 +2,21 @@
  * Search function
  * @description This function displays a search form when a button is clicked.
  */
+import "../utils/utils";
 
 export const search = () => {
 
   const btnSearch = document.querySelectorAll('.btn-toggle-search');
-  
-  btnSearch.forEach((thisBtnSearch) => {
-    const searchForm = document.querySelector('.header-search-form');
-    const searchOverlay = document.querySelector('.header-search-overlay');
-    const searchInput = document.querySelector('.header-search-form .form-control');
 
-    thisBtnSearch.addEventListener('click', (event) => {
-      window.setTimeout(() => { 
-        searchInput.focus(); 
+  btnSearch.forEach(thisBtnSearch => {
+    thisBtnSearch.addEventListener('click', event => {
+      window.setTimeout(function () { 
+        document.querySelector('.header-search-form .form-control').focus(); 
       }, 0); 
-      searchForm.classList.toggle('active');
-      searchOverlay.classList.toggle('active');
-      document.documentElement.classList.toggle('overflow-search');
-      document.body.classList.toggle('overflow-search');
+      document.querySelector('.header-search-form').slideToggle();
+      document.querySelector('.header-search-overlay').classList.toggle('active');
+      document.querySelector('html').classList.toggle('overflow-search');
+      document.querySelector('body').classList.toggle('overflow-search');
     });
   });
 
