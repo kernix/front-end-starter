4cf55cf61f0b6e141a83fe14e932e05598398b72
Jb PHAM <jbpham@kernix.com>
2024-11-20 12:37:34 +0100

    Update search.js

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -14,6 +14,15 @@ export const search = () => {
         document.querySelector('.header-search-form .form-control').focus(); 
       }, 0); 
       document.querySelector('.header-search-form').slideToggle();
+      const headerSearchForm = document.querySelector('.header-search-form');
+      const isAriaHidden = headerSearchForm.getAttribute('aria-hidden') === 'true';
+      headerSearchForm.setAttribute('aria-hidden', !isAriaHidden);
+      const currentTabIndex = headerSearchForm.getAttribute('tabindex');
+      if (currentTabIndex === '-1') {
+        headerSearchForm.setAttribute('tabindex', '0');
+      } else {
+        headerSearchForm.setAttribute('tabindex', '-1');
+      }
       document.querySelector('.header-search-overlay').classList.toggle('active');
       document.querySelector('html').classList.toggle('overflow-search');
       document.querySelector('body').classList.toggle('overflow-search');
